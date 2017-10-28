f6280b9dc98d9a38f1af8ea3b4261f804c9f2734
root <root@dndgen.puphpet>
2017-10-28 02:15:35 +0000

    first batch of migrations created

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,9 +1,45 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facade\DB;
 
 class DatabaseSeeder extends Seeder
 {
+	private $races = [
+		"Dwarf",
+		"Elf",
+		"Halfling",
+		"Human",
+		"Dragonborn",
+		"Gnome",
+		"Half-Elf",
+		"Half-Orc",
+		"Tiefling",
+		"Aasimar",
+		"Firbolg",
+		"Goliath",
+		"Kenku",
+		"Lizardfolk",
+		"Tabaxi",
+		"Triton",
+	];
+	
+	private $classes = [
+		"Barbarian",
+		"Bard",
+		"Cleric",
+		"Druid",
+		"Fighter",
+		"Monk",
+		"Paladin",
+		"Ranger",
+		"Rogue",
+		"Sorcerer",
+		"Warlock",
+		"Wizard",
+	];
+	
+	
     /**
      * Run the database seeds.
      *
@@ -12,5 +48,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+		foreach($races AS $race)
+		{
+			DB:table('races')->insert($race);
+		}
+		
+		foreach($classes AS $class)
+		{
+			DB:table('classes')->insert($class);
+		}
     }
 }
