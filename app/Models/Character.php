6c75e70471cfec2f124803394d529966c0883502
Josh Garnick <garnick@gmail.com>
2017-11-03 05:18:41 +0000

    changes and things

--- app/Models/Character.php
+++ app/Models/Character.php
@@ -4,7 +4,144 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+
 class Character extends Model
 {
-    //
+    protected $table = "characters";
+	
+	public function user()
+	{
+		return $this->belongsTo(\App\User::class);
+	}
+	
+	public function class()
+	{
+		return $this->belongsTo(\App\Models\CharacterClass::class)
+	}
+	
+	public function background()
+	{
+		return $this->belongsTo(\App\Models\Background::class)
+	}
+	
+	public function race()
+	{
+		return $this->belongsTo(\App\Models\Race::class)
+	}
+	
+	public function sub_race()
+	{
+		return $this->belongsTo(\App\Models\Subrace::class)
+	}
+	
+	public function prof_bonus()
+	{
+		if($this->level > 0 && $this->level < 5)
+		{
+			return 2;
+		}
+		
+		if($this->level > 4 && $this->level < 9)
+		{
+			return 3;
+		}
+		
+		if($this->level > 8 && $this->level < 13)
+		{
+			return 4;
+		}
+		
+		if($this->level > 12 && $this->level < 17)
+		{
+			return 5;
+		}
+		
+		if($this->level > 16 && $this->level < 21)
+		{
+			return 6;
+		}
+	}
+	
+	public function getAbilityModifier($stat)
+	{
+		if($stat === 1)
+		{
+			return -5;
+		}
+		
+		if($stat === 2 || $stat === 3 )
+		{
+			return -4;
+		}
+		
+		if($stat === 4 || $stat === 5 )
+		{
+			return -3;
+		}
+		
+		if($stat === 6 || $stat === 7 )
+		{
+			return -2;
+		}
+		
+		if($stat === 8 || $stat === 9 )
+		{
+			return -1;
+		}
+		
+		if($stat === 10 || $stat === 11 )
+		{
+			return 0;
+		}
+		
+		if($stat === 12 || $stat === 13 )
+		{
+			return 1;
+		}
+		
+		if($stat === 14 || $stat === 15 )
+		{
+			return 2;
+		}
+		
+		if($stat === 16 || $stat === 17 )
+		{
+			return 3;
+		}
+		
+		if($stat === 18 || $stat === 19 )
+		{
+			return 4;
+		}
+		
+		if($stat === 20 || $stat === 21 )
+		{
+			return 5;
+		}
+		
+		if($stat === 22 || $stat === 23 )
+		{
+			return 6;
+		}
+		
+		if($stat === 24 || $stat === 25 )
+		{
+			return 7;
+		}
+		
+		if($stat === 26 || $stat === 27 )
+		{
+			return 8;
+		}
+		
+		if($stat === 28 || $stat === 29 )
+		{
+			return 9;
+		}
+		
+		if($stat === 30 )
+		{
+			return 10;
+		}
+	}
 }
--- database/migrations/2017_11_03_044039_add_default_to_characters_table_ac.php
+++ database/migrations/2017_11_03_044039_add_default_to_characters_table_ac.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAttributesTable extends Migration
+class AddDefaultToCharactersTableAc extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateAttributesTable extends Migration
      */
     public function up()
     {
-        Schema::create('attributes', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('abbr');
-            $table->timestamps();
-        });
+        Schema::table('characters', function($table){
+			$table->integer('AC')->default(10)->change();
+		});
+		
+		Schema::table('characters', function($table){
+			$table->renameColumn('AC', 'ac');
+		});
     }
 
     /**
@@ -28,6 +29,6 @@ class CreateAttributesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('attributes');
+        //
     }
 }
