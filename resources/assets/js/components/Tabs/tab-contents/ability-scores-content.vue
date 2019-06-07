f9e94a7faef9898b42cb8538aa5b540159c13a0c
Josh Garnick <garnick.josh@gmail.com>
2019-06-06 23:13:00 -0700

    manual attribute input working

--- resources/assets/js/components/Tabs/tab-contents/ability-scores-content.vue
+++ resources/assets/js/components/Tabs/tab-contents/ability-scores-content.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="atts-container">
-        <v-att-selector v-for="(attOb, index) in charAtts" :attOb="attOb" :key="index"></v-att-selector>
+        <v-att-selector v-for="(attOb, index) in charAtts" :attOb="attOb" :key="index" @increaseAtt="increase" @decreaseAtt="decrease" @changeVal="changeVal" @validate="validate"></v-att-selector>
     </div>
 </template>
 
@@ -20,16 +20,73 @@ export default {
             ]
         }
     },
-    mounted(){
-        console.log(this.$store.state.data);
-    },
-    ...mapActions([ // spread operator so that other methods can still be added.
-        'changeStr',
-        'changeDex',
-        'changeCon',
-        'changeInt',
-        'changeWis',
-        'changeCha'
-    ]),
+    methods: {
+        ...mapActions([ // spread operator so that other methods can still be added.
+            'changeStr',
+            'changeDex',
+            'changeCon',
+            'changeInt',
+            'changeWis',
+            'changeCha'
+        ]),
+        increase(global, local){
+            //get the ability, get the value, check against min and max, update character ability
+            
+            let att = global.abbr.toLowerCase();
+            local.charVal++;
+            local.charVal = this.verifyRange(local.charVal);
+            this.changeCharAttribute(att, local.charVal);
+        },
+        decrease(global, local){
+            let att = global.abbr.toLowerCase();
+            local.charVal--;
+            local.charVal = this.verifyRange(local.charVal);
+            this.changeCharAttribute(att, local.charVal);
+        },
+        validate(local, data){
+            if( isNaN( parseInt(data) ) ){
+                local.charVal = local.charVal.slice(0, -1);
+                return;
+            }
+        },
+        changeVal(local, data, global){
+            let att = global.abbr.toLowerCase();
+            local.charVal = this.verifyRange(local.charVal);
+            this.changeCharAttribute(att, local.charVal);
+        },
+        changeCharAttribute(global, payload){
+            switch(global){
+                case "str":
+                    this.changeStr(payload);
+                    break;
+                case "dex":
+                    this.changeDex(payload);
+                    break;
+                case "con":
+                    this.changeCon(payload);
+                    break;
+                case "wis":
+                    this.changeWis(payload);
+                    break;
+                case "int":
+                    this.changeInt(payload);
+                    break;
+                case "cha":
+                    this.changeCha(payload);
+                    break;
+            }
+        },
+        verifyRange(value){
+            if(parseInt(value) > 18){
+                value = 18;
+            }
+
+            if(parseInt(value) < 8){
+                value = 8;
+            }
+
+            return value;
+        }
+    }
 }
 </script>
--- resources/assets/js/components/att-selector.vue
+++ resources/assets/js/components/att-selector.vue
@@ -1,11 +1,14 @@
 <template>
-<div class="att-input-container">
-    <input type="text" class="att-input" :name="gAtt.name" :value="attOb.charVal" @input="changeVal" @change="verifyRange">
-    <button class="increase" @click.stop="increaseVal"><span>+</span></button>
-    <button class="decrease" @click.stop="decreaseVal"><span>-</span></button>
+<div>
+    <div class="att-header">{{gAtt.abbr}}</div>
+    <div class="d-flex">
+        <div class="att-input-container">
+            <input type="text" class="att-input" :name="gAtt.name" @input="validate" @change="changeVal" v-model="attOb.charVal">
+            <button class="increase" @click.stop="increaseVal"><span>+</span></button>
+            <button class="decrease" @click.stop="decreaseVal"><span>-</span></button>
+        </div>
+    </div>
 </div>
-    
-    
 </template>
 
 <script>
@@ -17,30 +20,18 @@ export default {
             gAtt: this.$store.state.data.cattributes[this.attOb.attIndex],
         }
     },
-    mounted(){
-        
-    },
     methods: {
         increaseVal(el){
-            console.log(el.target);
-
+            this.$emit("increaseAtt", this.gAtt, this.attOb);
         },
         decreaseVal(el){
-
+            this.$emit("decreaseAtt", this.gAtt, this.attOb);
         },
         changeVal(el){
-            if( isNaN( parseInt(el.data) ) ){
-                el.target.value = el.target.value.slice(0, -1);
-            }
+            this.$emit("changeVal", this.attOb, el.data, this.gAtt);
         },
-        verifyRange(el){
-            if(parseInt(el.target.value) > 30){
-                el.target.value = 30;
-            }
-
-            if(parseInt(el.target.value) < 8){
-                el.target.value = 8;
-            }
+        validate(el){
+            this.$emit("validate", this.attOb, el.data);
         }
     },
     computed:{
